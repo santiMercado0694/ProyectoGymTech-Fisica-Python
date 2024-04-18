ef0d1bb6f48c4f65230ba58688f20bf954dd9525
diegojuarez965 <diegohernanjuarez965@gmail.com>
2024-04-18 10:32:15 -0300

    Commit2
    Origen y polo trasladado al codo
    Columna "tiempo(seg)" agregada a la salida
    Video con número de frame para análisis preciso

--- prueba.py
+++ prueba.py
@@ -10,6 +10,7 @@ video_path = 'videos/biscep2.mp4'
 output_video_path = 'resultados/tracked_video.mp4'
 output_csv_path = 'resultados/data.csv'
 output_polar_csv_path = 'resultados/polar_data.csv'
+FPS= 30
 
 cap = cv2.VideoCapture(video_path)
 
@@ -19,21 +20,21 @@ landmarks_of_interest = [mp_pose.PoseLandmark.LEFT_SHOULDER,
                          mp_pose.PoseLandmark.LEFT_WRIST]
 
 # Crear el DataFrame para almacenar los datos de la pose (coordenadas cartesianas)
-columns_cartesian = ['frame_number']
+columns_cartesian = ['frame_number','tiempo(seg)']
 for landmark in landmarks_of_interest:
     columns_cartesian.append(landmark.name + '_x')
     columns_cartesian.append(landmark.name + '_y')
 pose_data_cartesian = pd.DataFrame(columns=columns_cartesian)
 
 # Crear el DataFrame para almacenar los datos de las coordenadas polares
-columns_polar = ['frame_number']
+columns_polar = ['frame_number','tiempo(seg)']
 for landmark in landmarks_of_interest:
     columns_polar.append(landmark.name + '_r')
-    columns_polar.append(landmark.name + '_theta')
+    columns_polar.append(landmark.name + '_theta(rad)')
 pose_data_polar = pd.DataFrame(columns=columns_polar)
 
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (int(cap.get(3)), int(cap.get(4))))
+video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), FPS, (int(cap.get(3)), int(cap.get(4))))
 
 frame_number = 0
 
@@ -55,16 +56,26 @@ while cap.isOpened():
                               mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                               mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                               )
+    # Dibujar el número de frame (Para relacionar con precisión los datos obtenidos con el momento exacto del video)
+    cv2.putText(image, f'Frame: {frame_number}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     # Guardar el cuadro procesado en el video de salida
     video_writer.write(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
     # Recolectar los datos de la pose (coordenadas cartesianas) en el DataFrame
-    pose_row_cartesian = {'frame_number': frame_number}
+    pose_row_cartesian = {'frame_number': frame_number,'tiempo(seg)': frame_number/FPS}
     if results.pose_landmarks:
+        # Obtener las coordenadas del codo
+        elbow_x = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x
+        elbow_y = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y
+
         for landmark in landmarks_of_interest:
-            pose_row_cartesian[landmark.name + '_x'] = results.pose_landmarks.landmark[landmark].x
-            pose_row_cartesian[landmark.name + '_y'] = results.pose_landmarks.landmark[landmark].y
+            # Calcular las coordenadas relativas al codo
+            rel_x = results.pose_landmarks.landmark[landmark].x - elbow_x
+            rel_y = (results.pose_landmarks.landmark[landmark].y - elbow_y) * -1 
+
+            pose_row_cartesian[landmark.name + '_x'] = rel_x
+            pose_row_cartesian[landmark.name + '_y'] = rel_y
     else:
         for landmark in landmarks_of_interest:
             pose_row_cartesian[landmark.name + '_x'] = None
@@ -73,25 +84,27 @@ while cap.isOpened():
     pose_data_cartesian = pd.concat([pose_data_cartesian, pd.DataFrame([pose_row_cartesian])], ignore_index=True)
 
     # Calcular las coordenadas polares y almacenarlas en el DataFrame correspondiente
-    pose_row_polar = {'frame_number': frame_number}
+    pose_row_polar = {'frame_number': frame_number,'tiempo(seg)': frame_number/FPS}
     if results.pose_landmarks:
         for landmark in landmarks_of_interest:
-            x = results.pose_landmarks.landmark[landmark].x
-            y = results.pose_landmarks.landmark[landmark].y
-            r = np.sqrt(x**2 + y**2)  # Calcular el radio
-            theta = np.arctan2(y, x)  # Calcular el ángulo
+            rel_x = pose_row_cartesian[landmark.name + '_x']
+            rel_y = pose_row_cartesian[landmark.name + '_y']
+
+            r = np.sqrt(rel_x ** 2 + rel_y ** 2)  # Calcular el radio
+            theta = np.arctan2(rel_y, rel_x)  # Calcular el ángulo
             pose_row_polar[landmark.name + '_r'] = r
-            pose_row_polar[landmark.name + '_theta'] = theta
+            pose_row_polar[landmark.name + '_theta(rad)'] = theta
     else:
         for landmark in landmarks_of_interest:
             pose_row_polar[landmark.name + '_r'] = None
-            pose_row_polar[landmark.name + '_theta'] = None
+            pose_row_polar[landmark.name + '_theta(rad)'] = None
 
     pose_data_polar = pd.concat([pose_data_polar, pd.DataFrame([pose_row_polar])], ignore_index=True)
 
     frame_number += 1
 
 pose.close()
+video_writer.release()
 cap.release()
 
 # Guardar los DataFrames como archivos CSV
