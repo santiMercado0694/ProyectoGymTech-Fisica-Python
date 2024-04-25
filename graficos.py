bd64b540ea7b92c5ed472997a6205114e2dfaca8
diegojuarez965 <diegohernanjuarez965@gmail.com>
2024-04-25 15:17:43 -0300

    ConexionGraficosGUI
    1) Pasar coordenadas a metros siguiendo esta lógica:
    En el frame 155 se puede observar que la persona tiene el brazo completamente estirado. Analizando los datos de las coordenadas polares nos damos cuenta que la distancia entre el codo y la muñeca, es decir el largo del antebrazo es
    0.19 px. Teniendo en cuenta que el largo de antebrazo promedio es de 0.30 m, haciendo la regla de 3 nos queda que:
    Posición en metros= posición actual normalizada * 1.58
    2) Realizar un gráfico de gráficos y mostrarlo en la GUI una vez seleccionado el video
    3) Se agregó una imagen inicial
    4) En el video además del nro de frame se muestra el segundo actual, lo cual es útil para analizar los gráficos de abajo
    5) Una vez se cierra la ventana se cierra el programa porque quedaba corriendo en 2do plano

--- graficos.py
+++ graficos.py
@@ -1,55 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def generarGrafico(tiempo, posicion, string):
-    ruta = 'resultados/graficos/'+string+'.png'
-    #Crea una nueva figura
-    plt.figure()
-    plt.plot(tiempo, posicion, marker='o', linestyle='-')
-    plt.title(string)
-    plt.xlabel('Tiempo')
-    plt.ylabel('Metros')
-    plt.grid(True)
-    plt.savefig(ruta)
-    #plt.show()
+def generarSubgraficos(tiempo, datos, titulos, unidades):
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+    for i, ax in enumerate(axs.flat):
+        ax.plot(tiempo, datos[i], marker='o', linestyle='-')
+        ax.set_title(titulos[i])
+        ax.set_xlabel('Tiempo(seg)')
+        ax.set_ylabel(unidades[i])
+        ax.grid(True)
+    plt.tight_layout()
+    plt.savefig('resultados/graficos/subgraficos.png')
+    print("Grafico de subgraficos guardado en resultados/graficos/subgraficos.png")
 
 def createNewDataframe():
-    dataframe = pd.read_csv('resultados/documents/data.csv',index_col=[0])
-    #Nuevo dataframe solo con muñeca
-    df2=pd.DataFrame(dataframe.LEFT_WRIST_x)
-    df2['LEFT_WRIST_y']=dataframe['LEFT_WRIST_y']
+    dataframe = pd.read_csv('resultados/documents/data.csv', index_col=[0])
+    df2 = pd.DataFrame()
+    df2['posicion_x'] = dataframe['LEFT_WRIST_x(m)']
+    df2['posicion_y'] = dataframe['LEFT_WRIST_y(m)']
+    df2['tiempo'] = dataframe['tiempo(seg)']
 
-    #voy a suponer que, por ejemplo la mitad de la pantalla (0.5, porque está normalizado, va de 0 a 1) son 2 metros, aca usen la diferencia de posicion
-    #entre una articulación y otra, y mídanse, esto está de ejemplo.
-    #regla de 3. Si 0.5 son 2 metros, mi posición actual normalizada es ** metros. posición actual normalizada*2/0.5.
-    df2['posicion_x']=4*df2.LEFT_WRIST_x
-    df2['posicion_y']=4*df2.LEFT_WRIST_y
- 
-    #pdiferencia entre la posición y tiempo actual y el anterior. .diff hace eso.
-    df2['tiempo']=dataframe['tiempo(seg)']
     df2['dx'] = df2['posicion_x'].diff()
     df2['dy'] = df2['posicion_y'].diff()
     df2['dt'] = df2['tiempo'].diff()
 
-    #Quito las filas con nan (la primera, porque resto el valor anterior que no existe)
     df2.dropna(inplace=True)
 
-    #Calculo la velocidad
-    df2['vx']=df2['dx']/df2['dt']
-    df2['vy']=df2['dy']/df2['dt']
+    df2['vx'] = df2['dx'] / df2['dt']
+    df2['vy'] = df2['dy'] / df2['dt']
 
     tiempo = df2['tiempo']
-    posicion_x = df2['posicion_x']
-    posicion_y = df2['posicion_y']
-    velocidad_x = df2['vx']
-    velocidad_y = df2['vy']
+    datos = [df2['posicion_x'], df2['posicion_y'], df2['vx'], df2['vy']]
+    titulos = ['Posicion X Muneca', 'Posicion Y Muneca', 'Velocidad X Muneca', 'Velocidad Y Muneca']
+    unidades = ['m', 'm', 'm/s', 'm/s']
 
-    generarGrafico(tiempo,posicion_x, 'posicion_x_muneca')
-    generarGrafico(tiempo,posicion_y, 'posicion_y_muneca')
-    generarGrafico(tiempo,velocidad_x, 'velocidad_x_muneca')
-    generarGrafico(tiempo,velocidad_y, 'velocidad_y_muneca')
+    generarSubgraficos(tiempo, datos, titulos, unidades)
 
 createNewDataframe()
-
-
-
--- prueba.py
+++ prueba.py
@@ -13,9 +13,9 @@ def track_pose(video_path):
 
     VIDEO_PATH = video_path
     OUTPUT_VIDEO_PATH = 'resultados/video/tracked_video.mp4'
-    OUTPUT_CSV_PATH = 'resultados//documents/data.csv'
+    OUTPUT_CSV_PATH = 'resultados/documents/data.csv'
     OUTPUT_POLAR_CSV_PATH = 'resultados/documents/polar_data.csv'
-    FPS= 30
+    FPS = 30
 
     cap = cv2.VideoCapture(VIDEO_PATH)
 
@@ -25,21 +25,22 @@ def track_pose(video_path):
                             mp_pose.PoseLandmark.LEFT_WRIST]
 
     # Crear el DataFrame para almacenar los datos de la pose (coordenadas cartesianas)
-    columns_cartesian = ['frame_number','tiempo(seg)']
+    columns_cartesian = ['frame_number', 'tiempo(seg)']
     for landmark in landmarks_of_interest:
-        columns_cartesian.append(landmark.name + '_x')
-        columns_cartesian.append(landmark.name + '_y')
+        columns_cartesian.append(landmark.name + '_x(m)')
+        columns_cartesian.append(landmark.name + '_y(m)')
     pose_data_cartesian = pd.DataFrame(columns=columns_cartesian)
 
     # Crear el DataFrame para almacenar los datos de las coordenadas polares
-    columns_polar = ['frame_number','tiempo(seg)']
+    columns_polar = ['frame_number', 'tiempo(seg)']
     for landmark in landmarks_of_interest:
-        columns_polar.append(landmark.name + '_r')
+        columns_polar.append(landmark.name + '_r(m)')
         columns_polar.append(landmark.name + '_theta(rad)')
     pose_data_polar = pd.DataFrame(columns=columns_polar)
 
     pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-    video_writer = cv2.VideoWriter(OUTPUT_VIDEO_PATH, cv2.VideoWriter_fourcc(*'mp4v'), FPS, (int(cap.get(3)), int(cap.get(4))))
+    video_writer = cv2.VideoWriter(OUTPUT_VIDEO_PATH, cv2.VideoWriter_fourcc(*'mp4v'), FPS,
+                                    (int(cap.get(3)), int(cap.get(4))))
 
     FRAME_NUMBER = 0
 
@@ -61,47 +62,51 @@ def track_pose(video_path):
                                 mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                                 mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                                 )
-        # Dibujar el número de frame (Para relacionar con precisión los datos obtenidos con el momento exacto del video)
-        cv2.putText(image, f'Frame: {FRAME_NUMBER}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        # Dibujar el número de frame y segundo (Para relacionar con precisión los datos obtenidos con el momento exacto del video)
+        tiempo_segundos = FRAME_NUMBER / FPS
+        cv2.putText(image, f'Frame: {FRAME_NUMBER}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(image, f'Segundo: {tiempo_segundos:.2f}', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 255, 255), 2, cv2.LINE_AA)
 
         # Guardar el cuadro procesado en el video de salida
         video_writer.write(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
         # Recolectar los datos de la pose (coordenadas cartesianas) en el DataFrame
-        pose_row_cartesian = {'frame_number': FRAME_NUMBER,'tiempo(seg)': FRAME_NUMBER/FPS}
+        pose_row_cartesian = {'frame_number': FRAME_NUMBER, 'tiempo(seg)': tiempo_segundos}
         if results.pose_landmarks:
             # Obtener las coordenadas del codo
             elbow_x = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x
             elbow_y = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y
 
             for landmark in landmarks_of_interest:
-                # Calcular las coordenadas relativas al codo
-                rel_x = results.pose_landmarks.landmark[landmark].x - elbow_x
-                rel_y = (results.pose_landmarks.landmark[landmark].y - elbow_y) * -1 
+                # Calcular las coordenadas relativas al codo y pasarlas a metros
+                rel_x = (results.pose_landmarks.landmark[landmark].x - elbow_x) * 1.58
+                rel_y = (results.pose_landmarks.landmark[landmark].y - elbow_y) * -1.58
 
-                pose_row_cartesian[landmark.name + '_x'] = rel_x
-                pose_row_cartesian[landmark.name + '_y'] = rel_y
+                pose_row_cartesian[landmark.name + '_x(m)'] = rel_x
+                pose_row_cartesian[landmark.name + '_y(m)'] = rel_y
         else:
             for landmark in landmarks_of_interest:
-                pose_row_cartesian[landmark.name + '_x'] = None
-                pose_row_cartesian[landmark.name + '_y'] = None
+                pose_row_cartesian[landmark.name + '_x(m)'] = None
+                pose_row_cartesian[landmark.name + '_y(m)'] = None
 
         pose_data_cartesian = pd.concat([pose_data_cartesian, pd.DataFrame([pose_row_cartesian])], ignore_index=True)
 
         # Calcular las coordenadas polares y almacenarlas en el DataFrame correspondiente
-        pose_row_polar = {'frame_number': FRAME_NUMBER,'tiempo(seg)': FRAME_NUMBER/FPS}
+        pose_row_polar = {'frame_number': FRAME_NUMBER, 'tiempo(seg)': tiempo_segundos}
         if results.pose_landmarks:
             for landmark in landmarks_of_interest:
-                rel_x = pose_row_cartesian[landmark.name + '_x']
-                rel_y = pose_row_cartesian[landmark.name + '_y']
+                rel_x = pose_row_cartesian[landmark.name + '_x(m)']
+                rel_y = pose_row_cartesian[landmark.name + '_y(m)']
 
                 r = np.sqrt(rel_x ** 2 + rel_y ** 2)  # Calcular el radio
                 theta = np.arctan2(rel_y, rel_x)  # Calcular el ángulo
-                pose_row_polar[landmark.name + '_r'] = r
+                pose_row_polar[landmark.name + '_r(m)'] = r
                 pose_row_polar[landmark.name + '_theta(rad)'] = theta
         else:
             for landmark in landmarks_of_interest:
-                pose_row_polar[landmark.name + '_r'] = None
+                pose_row_polar[landmark.name + '_r(m)'] = None
                 pose_row_polar[landmark.name + '_theta(rad)'] = None
 
         pose_data_polar = pd.concat([pose_data_polar, pd.DataFrame([pose_row_polar])], ignore_index=True)
@@ -121,3 +126,4 @@ def track_pose(video_path):
     print("Datos de la pose (polares) guardados en:", OUTPUT_POLAR_CSV_PATH)
     if video_ready_callback:
         video_ready_callback()
+
