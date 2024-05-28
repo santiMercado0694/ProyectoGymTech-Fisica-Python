5d80f645e54e226d8a97cfd7b364da1db3073d57
Juan Cruz Pineiro <juancruzpineiro@gmail.com>
2024-05-28 16:15:59 -0300

    Cambios al grafico
    -Elimine generar subgraficos que no se usaba

--- guiCalculadora.py
+++ guiCalculadora.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import customtkinter as ctk
+import os 
 
 class VideoPlayerApp:
     frameNumber = 0
@@ -266,23 +267,18 @@ class VideoPlayerApp:
         self.image_label.configure(image=img_tk_with_line)
         self.image_label.image = img_tk_with_line
 
-    def generarSubgraficos(self, tiempo, datos, titulos, unidades):
-        fig, axs = plt.subplots(2, 2, figsize=(12, 8))
-        for i, ax in enumerate(axs.flat):
-            ax.plot(tiempo, datos[i], marker='o', linestyle='-')
-            ax.set_title(titulos[i])
-            ax.set_xlabel('Tiempo(seg)')
-            ax.set_ylabel(unidades[i])
-            ax.grid(True)
-        plt.tight_layout()
-        plt.savefig('resultados/graficos/subgraficos.png')
-        print("Grafico de subgraficos guardado en resultados/graficos/subgraficos.png")
+  
 
     def generarGraficos(self, tiempo, datos, titulos, unidades):
-        # Crear el directorio si no existe
+        if not os.path.exists('resultados/graficos'):
+            os.makedirs('resultados/graficos')
         for i, (dato, titulo, unidad) in enumerate(zip(datos, titulos, unidades)):
             fig, ax = plt.subplots(figsize=(6, 4))
-            ax.plot(tiempo, dato, marker='o', linestyle='-')
+            # Suavizar los datos con una interpolación cúbica
+            tiempo_suave = np.linspace(tiempo.min(), tiempo.max(), 500)
+            datos_suave = np.interp(tiempo_suave, tiempo, dato)
+            
+            ax.plot(tiempo_suave, datos_suave, linestyle='-', color='b')
             ax.set_title(titulo)
             ax.set_xlabel('Tiempo(seg)')
             ax.set_ylabel(unidad)
@@ -293,7 +289,7 @@ class VideoPlayerApp:
             plt.savefig(filename)
             print(f"Grafico guardado en {filename}")
             plt.close()
-
+            
     def createNewDataframe(self):
         dataframe = pd.read_csv('resultados/documents/data.csv', index_col=[0])
         df2 = pd.DataFrame()
